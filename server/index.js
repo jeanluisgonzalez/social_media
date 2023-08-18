6756f32c4215c4d5dda14f9f17ba49ad3c1d5a12
jeanluisgonzalez <jeanluiss26@gmail.com>
2023-08-17 23:00:13 -0400

    update at home

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/user.js";
-import postRoutes from "./routes/post.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
@@ -41,17 +41,16 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
+const upload = multer({ storage });
 
 // ROUTE WITH FILES
-app.post("/auth/register".upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTE
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
-
-const upload = multer({ storage });
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
